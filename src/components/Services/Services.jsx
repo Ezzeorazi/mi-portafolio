fe5034190aa5ec07491357775d52a72b084ee586
Ezzeorazi <ezequiel.orazi90gmail.com>
2024-10-06 13:18:15 -0300

    change: cards

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -5,27 +5,29 @@ import { FaReact, FaWordpress, FaDatabase } from "react-icons/fa";
 
 const Services = () => {
   return (
-	<>
-	<h2 className={styles.title}>Servicios</h2>
-    <div className={styles.services}>
-		
-      <ServiceCard
-        title="Páginas Web con React"
-        description="Desarrollo de aplicaciones web modernas y dinámicas utilizando React."
-        icon={<FaReact />}
-      />
-      <ServiceCard
-        title="Páginas Web con WordPress"
-        description="Creación y personalización de sitios web con WordPress."
-        icon={<FaWordpress />}
-      />
-      <ServiceCard
-        title="Bases de Datos con Spring Boot"
-        description="Implementación de bases de datos robustas y escalables con Spring Boot."
-        icon={<FaDatabase />}
-      />
-    </div>
-	</>
+    <>
+      <h2 className={styles.title}>Servicios</h2>
+
+      <div className={styles.services}>
+
+
+        <ServiceCard
+          title="Páginas Web con React"
+          description="Desarrollo de aplicaciones web modernas y dinámicas utilizando React."
+          icon={<FaReact />}
+        />
+        <ServiceCard
+          title="Páginas Web con WordPress"
+          description="Creación y personalización de sitios web con WordPress."
+          icon={<FaWordpress />}
+        />
+        <ServiceCard
+          title="Bases de Datos con Spring Boot"
+          description="Implementación de bases de datos robustas y escalables con Spring Boot."
+          icon={<FaDatabase />}
+        />
+      </div>
+    </>
   );
 };
 
