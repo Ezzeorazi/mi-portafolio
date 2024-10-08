58f5d6a77896bff517db0b71f3866fc650cfbb3c
Ezzeorazi <ezequiel.orazi90gmail.com>
2024-10-07 23:10:09 -0300

    create: new blog

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,10 @@ export * from './ProjectCard';
 export * from './Gallery';
 export * from './Experience';
 export * from './Loading'
-export * from './BlogEntry'
+// export * from './BlogEntry'
 export * from './BurgerButton'
 export * from './Education'
 export * from './Services'
 export * from './ServiceCard'
+export * from './ArticleList'
+export * from './ButtonList'
