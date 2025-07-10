bba0d7a2355e598395e7afba0eb3032276ce4cdd
Ezzeorazi <ezequiel.orazi90@gmail.com>
2025-07-10 05:02:27 -0300

    Changes new cursos

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -31,28 +31,46 @@ const Education = () => {
       description:
         "Estudié comunicación social con énfasis en periodismo y comunicación digital. Desarrollé habilidades de redacción, edición y producción de contenido para medios digitales e impresos. Realicé prácticas profesionales en medios de comunicación locales y participé en proyectos de investigación sobre medios digitales y redes sociales.",
     },
+    {
+      id: 5,
+      institution: "Universidad de Minnesota – Coursera",
+      degree: "Curso: Enterprise Systems (ERP y SAP)",
+      startDate: "Abril 2025",
+      endDate: "Junio 2025",
+      description:
+        "Curso introductorio sobre sistemas ERP, con enfoque en SAP y su aplicación en procesos empresariales. Estudié la estructura, funcionamiento y beneficios de los sistemas de planificación de recursos, incluyendo módulos clave como FI, CO y logística. Finalizado con certificación oficial de Coursera.",
+    },
 
+    {
+      id: 6,
+      institution: "Udemy",
+      degree: "Curso: Machine Learning con Python",
+      startDate: "Junio 2025",
+      endDate: "En curso",
+      description:
+        "Curso intensivo enfocado en los fundamentos del machine learning utilizando Python, NumPy, Pandas, Matplotlib y Scikit-learn. Incluye clasificación, regresión, árboles de decisión y redes neuronales. Aplicación práctica de algoritmos con datasets reales y desarrollo de modelos predictivos.",
+    },
     // Agrega más estudios aquí
   ];
   return (
     <>
       <h2 className={`animate__animated animate__slideInLeft ${styles.title}`}>Educación</h2>
       <div className={styles.educationContainer}>
-      {education.map((edu) => (
-        <div key={edu.id} 
-        className={`animate__animated animate__flipInX ${styles.educationCard}`}>
-          <h3>
-            {edu.institution}
-          </h3>
-          <p>
-            <strong>{edu.degree}</strong>
-          </p>
-          <p>
-            {edu.startDate} - {edu.endDate}
-          </p>
-          <p>{edu.description}</p>
-        </div>
-      ))}
+        {education.map((edu) => (
+          <div key={edu.id}
+            className={`animate__animated animate__flipInX ${styles.educationCard}`}>
+            <h3>
+              {edu.institution}
+            </h3>
+            <p>
+              <strong>{edu.degree}</strong>
+            </p>
+            <p>
+              {edu.startDate} - {edu.endDate}
+            </p>
+            <p>{edu.description}</p>
+          </div>
+        ))}
       </div>
     </>
   );
